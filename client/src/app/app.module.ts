72db9b4e8b1c872acfc7de57e26a921a4b9053bc
Nekimuntu <caribbeanfrenchschool@gmail.com>
2023-02-24 19:16:44 -0600

    End of section 11 error handling angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {HttpClientModule} from '@angular/common/http'  ///////SECTION 8 injecting services when Angular load
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'  ///////SECTION 8 injecting services when Angular load
 import { CoreModule } from './core/core.module';
 import { ShopComponent } from './shop/shop.component';
 import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,9 @@ import { HomeModule } from './home/home.module';
     CoreModule,
     HomeModule ////Section 10 
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
